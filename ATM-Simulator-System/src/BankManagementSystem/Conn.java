830077e13058066fb3232361240bae97af9ae9f0
SriDexit143 <128481534+SriDexit143@users.noreply.github.com>
2023-07-11 16:01:38 +0530

    Add files via upload

---
+++ ATM-Simulator-System/src/BankManagementSystem/Conn.java
@@ -0,0 +1,20 @@
+package BankManagementSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bms","root","1912");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
